293cd1e9ae2c227e6409bd393bff11556f0fb390
Read Langworthy <rlangworthy@gmail.com>
2022-02-10 12:03:36 -0800

    commit tree 12f85779b375f1b2ee42c8abb2fe56d0f68d69e5

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+
+
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator array_of_names
+    # array_of_messages = []
+    array_of_names.map {|name| badge_maker name }
+    
+end
+
+array = ["ryan", "lindsay", "emily"]
+
+# puts batch_badge_creator array
+
+def assign_rooms array_of_names
+    array_of_names.map {|name| "Hello, #{name}! You'll be assigned to room #{array_of_names.index(name) + 1}!"}
+    
+end
+
+#puts assign_rooms array
+
+def printer array_of_names
+    batch_badge_creator(array_of_names).each{|x| puts x}
+    assign_rooms(array_of_names).each{|x| puts x}
+    
+end
+
+
+printer array
